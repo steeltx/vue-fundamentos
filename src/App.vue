f9f7e9dea6cbbeee5fed2909a555ec55f84813cd
steeltx <realsteel.gnu@gmail.com>
2021-08-05 22:35:33 -0500

    Usando props

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Counter />
-  
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Counter :start="1" />
 </template>
 
 <script>
-import Counter from './components/Counter.vue'
+import Counter from "./components/Counter.vue";
 
 export default {
-  name: 'App',
-  components:{
-    Counter
-  }
-}
-
+  name: "App",
+  components: {
+    Counter,
+  },
+};
 </script>
 
 <style>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>Counter</h2>
+  <h2>{{ customTitle }}</h2>
   <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
   <div>
     <button @click="increase">+1</button>
@@ -9,25 +9,37 @@
 
 <script>
 export default {
+  props: {
+    title: {
+        type: String
+    },
+    start: {
+        type : Number,
+        default : 100
+    }
+  },
   data() {
     return {
-      counter: 5,
+      counter: this.start,
     };
   },
   methods: {
     getSquareValue() {
       return this.counter;
     },
-    increase(){
-        this.counter+=1;
+    increase() {
+      this.counter += 1;
     },
-    decrease(){
-        this.counter-=1;
-    }
+    decrease() {
+      this.counter -= 1;
+    },
   },
   computed: {
     squareCounter() {
       return this.counter * this.counter;
+    },
+    customTitle() {
+      return this.title || "Counter";
     },
   },
 };
@@ -35,18 +47,18 @@ export default {
 
 <style>
 button {
-    background-color: #64bb87;
-    border-radius: 5px;
-    border: 1px solid white;
-    color: white;
-    cursor: pointer;
-    margin: 0 5px;
-    padding: 5px 15px;
-    transition: 0.3s ease-in-out;
+  background-color: #64bb87;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
 }
 
-button:hover{
-    background-color: #5aa67b;
-    transition: 0.3s ease-in-out;
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
 }
 </style>
