dac4aa5c94b3f6d9e8973b34cf1140a0c3a47d06
steeltx <realsteel.gnu@gmail.com>
2021-08-05 21:50:30 -0500

    Inicio componente counter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Counter />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Counter
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Counter</h2>
+  <p> 10 </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
