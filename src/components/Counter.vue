756a1d1df2df4df5776295b4aa36489cc28d7310
steeltx <realsteel.gnu@gmail.com>
2021-08-05 22:14:25 -0500

    Boton incrementar y decrementar contador

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,28 +1,52 @@
 <template>
   <h2>Counter</h2>
-  <p> {{counter}} <sup>2</sup> = {{ squareCounter }} </p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            counter : 5
-        }
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  methods: {
+    getSquareValue() {
+      return this.counter;
     },
-    methods: {
-        getSquareValue(){
-            return this.counter
-        }
+    increase(){
+        this.counter+=1;
     },
-    computed: {
-        squareCounter() {
-            return this.counter * this.counter
-        }
+    decrease(){
+        this.counter-=1;
     }
-}
+  },
+  computed: {
+    squareCounter() {
+      return this.counter * this.counter;
+    },
+  },
+};
 </script>
 
 <style>
+button {
+    background-color: #64bb87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
 
+button:hover{
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
 </style>
