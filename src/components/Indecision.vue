83369310bd09ca1bff77aab7fe8b82a7bad39816
steeltx <realsteel.gnu@gmail.com>
2021-08-05 23:59:39 -0500

    Usando watch

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,66 +1,75 @@
 <template>
-  <img src="https://via.placeholder.com/250" alt="bg">
+  <img src="https://via.placeholder.com/250" alt="bg" />
   <div class="bg-dark"></div>
   <div class="indecision-container">
-      <input type="text" placeholder="Realiza una pregunta">
-      <p>Recuerda terminar con un signo de interrogación (?)</p>
+    <input type="text" placeholder="Realiza una pregunta" v-model="question" />
+    <p>Recuerda terminar con un signo de interrogación (?)</p>
 
-      <div>
-          <h2>Pregunta</h2>
-          <h1>Si, no</h1>
-      </div>
+    <div>
+      <h2>{{ question }}</h2>
+      <h1>Si, no</h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      question: null,
+    };
+  },
+  watch: {
+    question(value, oldValue) {
+      if (!value.includes("?")) return;
+    },
+  },
+};
 </script>
 
 <style scoped>
+img,
+.bg-dark {
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+}
 
-    img, .bg-dark {
-        height: 100vh;
-        left: 0px;
-        max-height: 100%;
-        max-width: 100%;
-        position: fixed;
-        top: 0px;
-        width: 100vw;
-    }
+.bg-dark {
+  background-color: rgba(0, 0, 0, 0.4);
+}
 
-    .bg-dark {
-        background-color: rgba(0, 0, 0, 0.4);
-    }
+.indecision-container {
+  position: relative;
+  z-index: 99;
+}
 
-    .indecision-container {
-        position: relative;
-        z-index: 99;
-    }
-    
-    input {
-        width: 250px;
-        padding: 10px 15px;
-        border-radius: 5px;
-        border: none;
-    }
-    input:focus {
-        outline: none;
-    }
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+input:focus {
+  outline: none;
+}
 
-    p {
-        color: white;
-        font-size: 20px;
-        margin-top: 0px;
-    }
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
+}
 
-    h1, h2 {
-        color: white;
-    }
-    
-    h2 {
-        margin-top: 150px;
-    }
+h1,
+h2 {
+  color: white;
+}
 
+h2 {
+  margin-top: 150px;
+}
 </style>
