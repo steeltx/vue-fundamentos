5b5f8e4c6a2c34b75975c88d0e80ae6737b09266
steeltx <realsteel.gnu@gmail.com>
2021-08-07 21:27:45 -0500

    Ejemplo de prueba con expect

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,18 +5,13 @@ describe('Example component', () => {
   test('Debe de ser mayor a 10', () => {
 
     // arreglar
-    let value = 5
+    let value = 10
 
     // estimulo
     value = value + 2
 
     // observar el resultado
-    if(value > 10){
-      // todo bien
-    }
-    else{
-      throw `${value} no es mayor a 10`
-    }
+    expect(value).toBeGreaterThan( 10 )
 
   })
 })
