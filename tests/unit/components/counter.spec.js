7b1ee492161627736138ec49897cc5a7f3725b66
steeltx <realsteel.gnu@gmail.com>
2021-08-08 18:52:55 -0500

    Evaluación de prop title

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -69,5 +69,20 @@ describe('Counter Component', () => {
 
     })
 
+    test('debe de mostrar la prop title', () => {
+
+        const title = 'Hola mundo'
+
+        const wrapper = shallowMount(Counter, {
+            props: {
+                title
+            }
+        })
+
+        expect(wrapper.find('h2').text()).toBe(title)
+        
+    })
+
+
 
 })
