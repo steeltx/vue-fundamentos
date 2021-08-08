3624e3a3f02973a9b6aff4aff34bd563dd56eda5
steeltx <realsteel.gnu@gmail.com>
2021-08-07 21:58:48 -0500

    Prueba con snapshot en Counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,7 @@
 <template>
   <h2>{{ customTitle }}</h2>
   <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
-  <div>
+  <div class="botones">
     <button @click="increase">+1</button>
     <button @click="decrease">-1</button>
   </div>
