368524d7ed25e3ceecfcc285a23ee05596eb3cfc
steeltx <realsteel.gnu@gmail.com>
2021-08-07 22:15:51 -0500

    Validar contenido de una etiqueta en Counter

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -3,11 +3,26 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
-    test('debe de hacer match con el snapshot', () => {
-        
+    // test('debe de hacer match con el snapshot', () => {
+
+    //     const wrapper = shallowMount(Counter)
+
+    //     expect( wrapper.html() ).toMatchSnapshot()
+    // })
+
+    test('h2 debe de tener el valor por defecto "Counter"', () => {
+
         const wrapper = shallowMount(Counter)
 
-        expect( wrapper.html() ).toMatchSnapshot()
+        // verificar que exista el elemento
+        expect(wrapper.find('h2').exists()).toBeTruthy()
+
+        // obtener el contenido del elemento
+        const h2Value = wrapper.find('h2').text()
+
+        expect(h2Value).toBe('Counter')
+
     })
-    
+
+
 })
