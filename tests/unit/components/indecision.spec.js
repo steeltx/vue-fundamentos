67efcdaae022f1d58bef5d824a743a875d00d69a
steeltx <realsteel.gnu@gmail.com>
2021-08-14 23:54:32 -0500

    Prueba de console.log con spy

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -3,18 +3,24 @@ import Indecision from "@/components/Indecision.vue"
 
 describe('Indecision component', () => {
 
-    let wrapper;
+    let wrapper
+    let clgSpy
 
     beforeEach(() => {
         wrapper = shallowMount(Indecision)
+        clgSpy = jest.spyOn(console,'log')
     })
 
     test('debe de hacer match con el snapshot', () => {  
         expect(wrapper.html()).toMatchSnapshot()
     })
 
-    test('escribir en el input no debe disparar nada (console.log)', () => {
+    test('escribir en el input no debe disparar nada (console.log)', async () => {
         
+        const input = wrapper.find('input')
+        await input.setValue('Hola')
+        expect(clgSpy).toHaveBeenCalledTimes(1)
+
     })
 
     test('escribir el simbolo de ? debe de disparar el fetch', () => {
