ebde909bdbf5a856ae718af27fd0bc5b66d241aa
steeltx <realsteel.gnu@gmail.com>
2021-08-14 21:16:07 -0500

    Definición de las pruebas a realizar en componente Indecision

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -13,4 +13,20 @@ describe('Indecision component', () => {
         expect(wrapper.html()).toMatchSnapshot()
     })
 
+    test('escribir en el input no debe disparar nada (console.log)', () => {
+        
+    })
+
+    test('escribir el simbolo de ? debe de disparar el fetch', () => {
+        
+    })
+
+    test('pruebas en getAnswer', () => {
+        
+    })
+    
+    test('pruebas en getAnswer - Fallo en el API', () => {
+        
+    })
+
 })
