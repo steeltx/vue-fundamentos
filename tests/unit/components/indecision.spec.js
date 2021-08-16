648831b1647340e09b09ebd2d94807946a63854f
steeltx <realsteel.gnu@gmail.com>
2021-08-15 19:33:49 -0500

    Pruebas en fetch api

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -6,7 +6,13 @@ describe('Indecision component', () => {
     let wrapper
     let clgSpy
 
-    global.fetch = jest.fn()
+    global.fetch = jest.fn( () => Promise.resolve({
+        json: () => Promise.resolve({
+            answer: 'yes',
+            forced: false,
+            image: 'https://yesno.wtf/assets/yes/2.gif'
+        })
+    }))
 
     beforeEach(() => {
         wrapper = shallowMount(Indecision)
@@ -43,7 +49,13 @@ describe('Indecision component', () => {
         
     })
 
-    test('pruebas en getAnswer', () => {
+    test('pruebas en getAnswer', async () => {
+        
+        await wrapper.vm.getAnswer()
+        const img = wrapper.find('img')
+        expect(img.exists()).toBeTruthy()
+        expect(wrapper.vm.img).toBe('https://yesno.wtf/assets/yes/2.gif')
+        expect(wrapper.vm.answer).toBe('Si!')
         
     })
     
